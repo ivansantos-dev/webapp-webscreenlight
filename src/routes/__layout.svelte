d24cd287867fffb60a84e22efca4ba6234b2cbfb
Ivan Santos <me@ivan-santos.dev>
2022-05-29 21:51:49 -0600

    Initial Web Monitor Light app

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	const requestFullScreen = () => {
+		// Supports most browsers and their versions.
+		let element = document.documentElement;
+		var requestMethod =
+			element.requestFullScreen ||
+			element.webkitRequestFullScreen ||
+			element.mozRequestFullScreen ||
+			element.msRequestFullScreen;
+
+		if (requestMethod) {
+			// Native full screen.
+			requestMethod.call(element);
+		} else if (typeof window.ActiveXObject !== 'undefined') {
+			// Older IE.
+			var wscript = new ActiveXObject('WScript.Shell');
+			if (wscript !== null) {
+				wscript.SendKeys('{F11}');
+			}
+		}
+	};
+</script>
+
+<div class="container">
+	<header
+		class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"
+	>
+		<h1>Web Monitor Light</h1>
+
+		<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+			<li class="nav-item">
+				<a href="#" on:click={requestFullScreen} class="nav-link">Go Fullscreen</a>
+			</li>
+			<li class="nav-item">
+				<a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" class="nav-link">FAQ</a>
+			</li>
+		</ul>
+	</header>
+	<main>
+		<slot />
+	</main>
+	<div class="container fixed-bottom">
+		<footer
+			class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
+		>
+			<p class="col-md-4 mb-0 text-muted">
+				&copy; 2022 <a href="http://ivan-santos.dev" target="_blank" class="text-decoration-none"
+					>Ivan Santos</a
+				>
+			</p>
+		</footer>
+	</div>
+</div>
+
+<div
+	class="modal fade"
+	id="exampleModal"
+	tabindex="-1"
+	aria-labelledby="exampleModalLabel"
+	aria-hidden="true"
+>
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">FAQ</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+			</div>
+			<div class="modal-body">
+				<p><strong>Web Monitor Light</strong> is a simple tool to light whatever you need.</p>
+
+				<p>Use-cases:</p>
+				<ul>
+					<li>source of light for videos</li>
+					<li>background light to copy drawing</li>
+					<li>black your screen in order to clean it</li>
+					<li>test monitor</li>
+				</ul>
+			</div>
+			<div class="modal-footer">
+				<p>
+					Created by <a href="http://ivan-santos.dev" target="_blank" class="link-unstyled"
+						>Ivan Santos</a
+					>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	let color = '#FFFFFF';
+	let presetColor: string;
+
+	const presetColors = [
+		{
+			name: 'white',
+			hex: '#FFFFFF'
+		},
+		{
+			name: 'black',
+			hex: '#000000'
+		},
+		{
+			name: 'warm white',
+			hex: '#FDF4DC'
+		},
+		{
+			name: 'daylight',
+			hex: '#F4E99B'
+		},
+		{
+			name: 'cool white',
+			hex: '#F4FDFF'
+		}
+	];
+
+	const selectPresetColor = () => {
+		document.body.style.backgroundColor = presetColor;
+		color = presetColor;
+	};
+
+	const changeBackgroundColor = () => {
+		document.body.style.backgroundColor = color;
+	};
+</script>
+
+<div class="row mb-3">
+	<div class="col-sm-2">
+		<label for="colorPreset" class="col-form-label">Color Presets</label>
+	</div>
+	<div class="col-sm-2">
+		<select
+			name="colorPreset"
+			id="colorPreset"
+			bind:value={presetColor}
+			on:change={selectPresetColor}
+			class="form-select mb-3"
+		>
+			{#each presetColors as color}
+				<option value={color.hex}>
+					{color.name}
+				</option>
+			{/each}
+		</select>
+	</div>
+</div>
+
+<label for="color" class="col-sm-2 col-form-label">Color Picker</label>
+<input type="color" name="color" id="color" bind:value={color} on:change={changeBackgroundColor} />
